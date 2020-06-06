6fc070d4aa499445095e0a33b186878ff236db55
Monty McCasland <momcas6166@gmail.com>
2020-06-06 17:15:22 -0500

    Init new project

--- android/app/src/main/java/com/reactnativetemplate/MainActivity.java
+++ android/app/src/main/java/com/reactnativetemplate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativetemplate;
+package com.reactnativetimer;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeTemplate";
+    return "ReactNativeTimer";
   }
 }
--- ios/ReactNativeTemplateTests/ReactNativeTemplateTests.m
+++ ios/ReactNativeTemplateTests/ReactNativeTemplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeTemplateTests : XCTestCase
+@interface ReactNativeTimerTests : XCTestCase
 
 @end
 
-@implementation ReactNativeTemplateTests
+@implementation ReactNativeTimerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
