73c2cb9dc22ffeed3465ff476bbd671a2bf9dcbb
Monty McCasland <momcas6166@gmail.com>
2020-06-24 12:30:18 -0500

    Update theme type

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -3,6 +3,7 @@ import "styled-components/native"
 declare module "styled-components" {
 	export interface DefaultTheme {
 		accentColor: string
+		dimTextColorOnPrimary: string
 		primaryColor: string
 		secondaryColor: string
 		textColorOnPrimary: string
